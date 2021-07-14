a8b75fbd8630c07e69bb50d2896e1eeffe0ece3c
Justin Joseph <jmjoseph20@hotmail.com>
2021-07-14 15:03:40 -0400

    Add project files.

---
+++ quiz-2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace quiz_2
+{
+    //#2
+    //public class Rectangle
+    //{
+    //    public int left = 0;
+    //    public int top = 0;
+    //    public int right = 0;
+    //    public int bottom = 0;
+    //    public Rectangle(int left, int top, int right, int bottom)
+    //    {
+    //        this.left = left;
+    //        this.top = top;
+    //        this.right = right;
+    //        this.bottom = bottom;
+    //    }
+    //    public Rectangle(int left, int top)
+    //    {
+    //        this.left = left;
+    //        this.top = top;
+    //        right = left;
+    //        bottom = top;
+    //    }
+    //not finished
+
+    //#7
+    //public class Fruit
+    //{
+    //    private string name { get; set; }
+
+    //    public Fruit(string name)
+    //    {
+    //        this.name = name;
+    //    }
+    //}
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //#1
+            //this keyword refers to the current instance of the class. It also helps
+            //you differentiate between the parameters from the constructor and the
+            //fields you have created in that class.
+
+            //#3
+            //a static member is callable on a class when no instance of the class has been created.
+            //Can be used to store a value that is shared among all instances of a class.
+
+            //#4
+
+
+            //#5
+            //public class Vehicle
+            //{
+            //public string manufacturer;
+            //public int maxSpeed;
+            //}
+
+            //#6
+            //1. Dictionary
+            //2. List
+            //3. List
+
+            //#8
+            //== is a comparison operator used to compare 
+            //.equals() is a method operator that
+
+            //#9
+            //private, public, protected, internal.
+            //private only accessible in the class
+            //public accessible from anywhere in the code.
+            //protected accessible only in the class and in other classes derived from that class.
+            //internal accessible only in it's own assembly.
+
+            //#10
+            //Which pair of words/phrases best completes the following sentence?
+            //Interfaces function as _________, specifying available methods without dictating _________
+            //a.d.labels, fields and properties
+            //b. abstract classes, constructor details
+            //c.templates, data types ------------------------------
+            //d.contracts, implementation details
+
+            //#11
+            //can't be instantiated. used to be inherited by a subclass that implements or overrides it's methods.
+
+            //#12
+            //allows us to send simple text commands to our computer. can be faster than using a gui or multiple guis.
+
+            //#13
+
+
+
+
+
+
+        }
+    }
+}
